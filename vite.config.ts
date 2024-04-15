e2bab762708af5cdda81365416b0a4eb016db9db
vitorbisterso <vitorbisterso@gmail.com>
2024-04-15 16:41:27 -0300

    Add github pages package

--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,6 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: [{ find: "@", replacement: resolve(__dirname, "./src") }]
-  }
+  },
+  base: '/tractian-tech-challenge'
 })
